c84a41e45a34fd2a45e361dbcf8cf8b3aa8ae7da
Reese Netro <reesie.netro@gmail.com>
2016-09-10 18:38:40 -0700

    ?

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+//globals
+var map;
+
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: {lat: 37.784701, lng: -122.397174} //37.784701, -122.397174
+  //directionsDisplay.setPanel('bottom');
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: {lat: 37.784701, lng: -122.397174}, //37.784701, -122.397174,
+    styles: [
+      {
+        featureType: 'all',
+        stylers: [
+          { saturation: -30 }
+        ]
+      }, {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [
+          {saturation: 100} ,
+          {hue: '#FB3C95'}
+        ]
+      }, {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [
+          {saturation: 100} ,
+          {hue: '#82F41E'}
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [
+          { hue: '#22CCCC' }
+        ]
+      },{
+        featureType: 'poi.business',
+        elementType: 'geometry',
+        stylers: [
+          {saturation: 100} ,
+          { hue: '#FBC43C' }
+        ]
+      },{
+        featureType: 'landscape',
+        elementType: 'geometry',
+        stylers: [
+          {hue: '#FBC43C'}
+        ]
+      } ]
   });
   directionsDisplay.setMap(map);
   document.getElementById('submit').addEventListener('click', function() {
@@ -14,13 +58,13 @@ function initMap() {
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   var waypts = [];
   var checkboxArray = document.getElementById('waypoints');
-  marker = new google.maps.Marker({
-    map: map,
-    draggable: false,
-    animation: google.maps.Animation.DROP,
-    position: {lat: 37.787983, lng: -122.406835} //37.787983, -122.406835
-  });
-  marker.addListener('click', toggleBounce);
+  // marker = new google.maps.Marker({
+  //   map: map,
+  //   draggable: false,
+  //   animation: google.maps.Animation.DROP,
+  //   position: {lat: 37.787983, lng: -122.406835} //37.787983, -122.406835
+  // });
+  // marker.addListener('click', toggleBounce);
   for (var i = 0; i < checkboxArray.length; i++) {
     if (checkboxArray.options[i].selected) {
       waypts.push({
@@ -31,7 +75,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   }
 
   directionsService.route({
-    origin: document.getElementById('start').value,
+    origin: {lat: 37.784701, lng: -122.397174}, //37.759790, -122.426846
     destination: document.getElementById('end').value,
     waypoints: waypts,
     optimizeWaypoints: true,
@@ -48,8 +92,8 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         var routeSegment = i + 1;
         summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
             '</b><br>';
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+        //summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        //summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
         summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
       }
     } else {
@@ -58,30 +102,3 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   });
 }
 
-function get_location(){
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    map.setCenter(pos);
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-}
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-          'Error: The Geolocation service failed.' :
-          'Error: Your browser doesn\'t support geolocation.');
-}
